193345655ad85a0b011e1cc5d2b90a7d368e2dc1
raphaelnishimura <raphael.nishimura@gmail.com>
2022-08-14 01:05:27 -0400

    Codigo

---
+++ Distribuicao de renda domiciliar em faixas de SM para pessoas com 16 anos ou mais da PNADC 2021 5a entrevista.R
@@ -0,0 +1,30 @@
+# Distribuicao de renda domiciliar em faixas de SM para pessoas com 16 anos ou mais da PNADC 2021 5a entrevista
+
+# 1. carrea pacotes ----
+library(PNADcIBGE)
+library(tidyverse)
+library(survey)
+library(srvyr)
+
+
+# 2. opcoes do pacote survey ----
+options(survey.lonely.psu = "adjust")
+
+
+# 3. baixa dados da PNADC 2021 - 5a entrevista ----
+pnadc_2021.5 <- get_pnadc(year = 2021, interview = 5, vars = c("Estrato","UPA","V1032","V2009","VD5010"), 
+                          defyear = 2021, design = FALSE)
+
+# 4. calcula distribuicao de renda domiciliar por faixas de SM para pessoas com 16 anos ou mais ---- 
+pnadc_2021.5 %>% 
+  filter(V2009 >= 16) %>%  # filtra pessoas com 16 anos ou mais
+  as_survey(ids = UPA, strata = Estrato, weights = V1032) %>% # define desenho amostral 
+  mutate(renda = VD5010 * CO1) %>% # calcula renda domiciliar deflacionada  
+  mutate(faixa_renda = case_when(  # categoriza renda domiciliar em faixas de SM (SM em 2021 = R$1100)
+    renda < (1100 * 2) ~ 1, 
+    renda >= (1100 * 2) & renda < (1100 * 5) ~ 2,
+    renda >= (1100 * 5) ~ 3),
+    renda_familiar = factor(faixa_renda, levels = c(1,2,3), labels = c("At? 2 SM", "De 2 at? 5 SM", "Acima de 5 SM"))) %>%
+  group_by(faixa_renda) %>% 
+  summarise(proportion = survey_mean(na.rm = T)) %>% 
+  filter(!is.na(faixa_renda))
